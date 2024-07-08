08ae2b3ba8e8449b0ee98d4881c27500dca3c5cc
Assil Tekaya <tekayaassil8@gmail.com>
2024-07-08 22:06:59 +0100

    home updates

---
+++ scripts.js
@@ -0,0 +1,14 @@
+(function ($) {
+    'use strict';
+
+    $(document).ready(function () {
+        /* Preloader */
+        $(window).on('load', function() {
+            $('body').addClass('loaded');
+        });
+
+        // Other JavaScript code here
+
+    });
+
+})(jQuery);
