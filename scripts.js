e872bc2bf80113d0a5ce0d0d95e7ce0fec15effc
Assil Tekaya <tekayaassil8@gmail.com>
2024-07-24 19:10:01 +0100

    update and about me section started

--- scripts.js
+++ scripts.js
@@ -5,29 +5,32 @@
         console.log("Document ready");
 
         /* Preloader */
-        $(window).on('load', function() {
+        $(window).on('load', function () {
             console.log("Window loaded");
             $('body').addClass('loaded');
+
+
+
+            /* Scroll Naviagation Background Change with Sticky Navigation */
+
+            $(window).on('scroll', function () {
+                if ($(window).scrollTop() > 100) {
+                    $('.header-top-area').addClass('navigation-background');
+                } else {
+                    $('.header-top-area').removeClass('navigation-background');
+                }
+            });
+
+
 
             /* Typed.js */
             console.log("Initializing typed.js");
             $(".typing").typed({
-                strings: ["","","I am Assil Tekaya", "A computer Science student", "Welcome to my Portfolio !"],    
+                strings: ["", "", "I am Assil Tekaya", "A computer Science student", "Welcome to my Portfolio !"],
                 typeSpeed: 50
             });
         });
 
-        // Ensure the preloader is removed after a timeout, as a fallback
-        setTimeout(function() {
-            if (!$('body').hasClass('loaded')) {
-                console.log("Fallback: Forcing loaded state");
-                $('body').addClass('loaded');
-                $(".typing").typed({
-                    strings: ["I am A Freelancer.", "I Design Websites.", "I Live in New York."],    
-                    typeSpeed: 50
-                });
-            }
-        }, 5000); // 5 seconds fallback
     });
 
 })(jQuery);
